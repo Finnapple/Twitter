f90919185fcfc614f17f84dfb4dd44a93e0b458e
Finnapple <johnlagriada@protonmail.com>
2026-04-28 01:29:24 +0800

    Update router.php

--- router.php
+++ router.php
@@ -11,7 +11,7 @@ if (preg_match('/\.(json|txt)$|\/register\.php$/', $uri)) {
     <title>SUP, HACKER</title>
     <meta name="description" content="flag{h3y_st4wp_r1ght_h3r3}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="love.png">
+    <link rel="icon" type="image/png" href="twitter.png">
     <style>
       html, body {
         margin: 0;
